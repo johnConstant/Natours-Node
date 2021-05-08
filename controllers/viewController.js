8db01b36871a14d34ec4b11beb5043083f8b8f94
John Constant <john@johns-mbp.home>
2021-05-08 16:38:57 +0100

    All tours page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,15 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+const Tour = require('../models/tourModel');
+
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // Get tour data
+  const tours = await Tour.find();
+  // Build template
+
+  //Render Template using tour data
+  res.status(200).render('overview', { title: 'All Tours', tours });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The Forest Hiker' });
