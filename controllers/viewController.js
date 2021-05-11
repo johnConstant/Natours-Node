5055010fb3f22b29178890e4a0c312ca10be8667
John Constant <john@johns-mbp.home>
2021-05-11 23:25:47 +0100

    A Tour Page PUG template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -11,6 +11,10 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   res.status(200).render('overview', { title: 'All Tours', tours });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', { title: 'The Forest Hiker' });
-};
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  res.status(200).render('tour', { title: tour.name, tour });
+});
